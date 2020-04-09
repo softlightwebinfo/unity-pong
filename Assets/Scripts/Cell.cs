9e5e92d4a042295338f8500037c1e3921a042afc
Rafa Gonzalez <softlightwebinfo@gmail.com>
2020-04-09 19:47:31 +0200

    Add buscaminas

---
+++ Assets/Scripts/Cell.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cell : MonoBehaviour
+{
+    public bool hasMine;
+    public Sprite[] emptyTextures;
+    public Sprite mineTexture;
+
+    void Start()
+    {
+        this.hasMine = (Random.value < 0.15);
+        // Obtener la columna;
+        int x = (int)this.transform.position.x;
+        //Obtenemos la fila que la tiene el padre;
+        int y = (int)this.transform.parent.transform.position.y;
+
+        GridHelper.cells[x, y] = this;
+    }
+
+    private void OnMouseUpAsButton()
+    {
+        if (this.hasMine)
+        {
+
+        }
+        else
+        {
+
+        }
+    }
+
+    public bool IsCovered()
+    {
+        return this.GetComponent<SpriteRenderer>().sprite.texture.name == "Panel";
+    }
+
+    public void loadTexture(int adjacentCount)
+    {
+        if (this.hasMine)
+        {
+            GetComponent<SpriteRenderer>().sprite = this.mineTexture;
+        }
+        else
+        {
+            GetComponent<SpriteRenderer>().sprite = this.emptyTextures[adjacentCount];
+        }
+    }
+}
---
+++ Assets/Scripts/GridHelper.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GridHelper : MonoBehaviour
+{
+    public static int w = 15;
+    public static int h = 15;
+    public static Cell[,] cells = new Cell[w, h];
+
+    void Start()
+    {
+
+    }
+}
